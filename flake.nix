e754c9ed1d7731650ec1add15b26fc835cae4ed3
Ying Xuan Eng <engyingxuan@gmail.com>
2024-10-27 22:49:28 -0400

    hello world, nix

---
+++ flake.nix
@@ -0,0 +1,99 @@
+{
+  description = "yxeng nix-darwin config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      home-manager,
+      nixpkgs,
+    }:
+    let
+      configuration =
+        { pkgs, ... }:
+        {
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = with pkgs; [
+            ispell
+            nixfmt-rfc-style
+            ripgrep
+          ];
+
+          homebrew = {
+            enable = true;
+            onActivation.autoUpdate = false;
+            # onActivation.cleanup = "uninstall";
+
+            taps = [ ];
+            brews = [ ];
+            casks = [ ];
+          };
+
+          users.users.yingxuan = {
+            name = "yingxuan";
+            home = "/Users/yingxuan";
+          };
+
+          nix.settings.trusted-users = [ "yingxuan" ];
+
+          # use touch id for sudo
+          security.pam.enableSudoTouchIdAuth = true;
+
+          # Auto upgrade nix package and the daemon service.
+          services.nix-daemon.enable = true;
+          # nix.package = pkgs.nix;
+
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          # Create /etc/zshrc that loads the nix-darwin environment.
+          programs.zsh.enable = true; # default shell on catalina
+          # programs.fish.enable = true;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 5;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#yxeng-macbook
+      darwinConfigurations."yxeng-macbook" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.yingxuan = import ./home-manager.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+
+        ];
+      };
+
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."yxeng-macbook".pkgs;
+    };
+}
---
+++ home-manager.nix
@@ -0,0 +1,71 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "yingxuan";
+  home.homeDirectory = "/Users/yingxuan";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs = {
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+      nix-direnv.enable = true;
+    };
+
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    # CR-someday yieng: all this seems oddly specific to mac, and so it
+    # shouldn't belong in home manager
+    git = {
+      enable = true;
+      userName = "Ying Xuan Eng";
+      userEmail = "engyingxuan@gmail.com";
+      signing.key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILahCDtkc84c6FuYxWBCE05O3stfRJIbm0yFh6ZlWIjq";
+      ignores = [ ".DS_Store"];
+      extraConfig = {
+        gpg.format = "ssh";
+        gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+        commit.gpgsign = true;
+      };
+    };
+
+    starship = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    zsh = {
+      enable = true;
+      shellAliases = {
+        switch = "darwin-rebuild switch --flake ~/.config/nix-darwin";
+        ec = "emacsclient -a '' -c -t";
+      };
+    };
+
+    ssh = {
+      enable = true;
+      extraConfig = ''
+          Host *
+          IdentityAgent ~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock
+    '';
+    };
+
+  };
+}
